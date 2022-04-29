7c0aaa5ec77dc75211ca3bedb4ae57ffab91d9f4
idsme <idsmesales@gmail.com>
2022-04-29 16:48:30 +0200

    feature/implemented connect and disconnect from wallet by requesting the account and removing accounts from frontend state.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import react, {useState, useEffect} from 'react';
 import './App.css';
+import detectEthereumProvider from '@metamask/detect-provider';
+import Calendar from './Calendar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [account, setAccount] = useState(false);
+
+    useEffect(() => {
+        isConnected();
+    }, []);
+
+    const isConnected = async () => {
+        console.log('isConnected');
+        const provider = await detectEthereumProvider();
+        const accounts = await provider.request({ method: "eth_accounts" });
+
+        if (accounts.length > 0) {
+            setAccount(accounts[0]); // the currently selected address is the first in the array returned.
+        } else {
+            console.log("No authorized account found")
+        }
+    }
+
+    const disconnect = async () => {
+        console.log("Disconnecting...");
+        const provider = await detectEthereumProvider();
+        await provider.request({
+            method: "eth_requestAccounts",
+            params: [{eth_accounts: {}}]
+        })
+        setAccount(false);
+    }
+
+    const connect = async () => {
+        try {
+            const provider = await detectEthereumProvider();
+
+            // returns an array of accounts
+            const accounts = await provider.request({ method: "eth_requestAccounts" });
+
+            // check if array at least one element
+            if (accounts.length > 0) {
+                console.log(`Accounts found: ${accounts}`);
+                setAccount(accounts[0]);
+            } else {
+                setAccount('No accounts found');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>calend3</h1>
+                <p id="slogan">web3 appointment scheduler</p>
+            </header>
+            {!account ? <button onClick={connect}>connect to wallet</button>: <button onClick={disconnect}>disconnect from wallet</button>}
+            {account && <div><Calendar></Calendar></div>}
+            {account}
+        </div>
+    );
 }
 
 export default App;
