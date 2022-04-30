8b2ffdf6c0fdcbe0a64ea137426692f49c48820e
idsme <idsmesales@gmail.com>
2022-04-30 19:47:20 +0200

    feature/implemented add more validation functionality .. which does not allow for appointments to overlap.

--- contracts/Calend3.sol
+++ contracts/Calend3.sol
@@ -1,14 +1,88 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Calend3 {
 
     uint rate = 1;
     address owner;
 
+    struct Appointment {
+        string title;     // title of the meeting
+        address attendee; // person you are meeting
+        uint startTime;   // start time of meeting
+        uint endTime;     // end time of the meeting
+        uint amountPaid;  // amount paid for the meeting
+    }
+
+    Appointment[] appointments;
+
     constructor(uint _rate) {
-        owner = msg.sender;
+        owner = payable(msg.sender);
         rate = _rate;
+    }
+
+    // TODO IDSME make it return something.
+    function createAppointment(string memory _title, uint _startTime, uint _endTime, uint _amountPaid) public payable returns (Appointment memory) {
+        // TODO IDSME Do some require checks.. require(msg.sender == owner, "Only owner can change rate"); for each argument.
+        uint  minimum_payment_amount = (_endTime - _startTime) * rate;
+        if (msg.sender != owner) { // If owner blocks agenda he should not have to pay. (oke only gas)
+            revert('owner cannot book an appointment');
+        }
+        if (_startTime >= _endTime) {
+            revert('start time must be before end time');
+        }
+        if (_amountPaid < minimum_payment_amount) {
+            string memory error_message = string(bytes.concat(bytes("You need to pay at least: "), bytes(Strings.toString(minimum_payment_amount)), bytes(", you tried to pay: "), bytes(Strings.toString(_amountPaid)), bytes(" WEI")));
+            revert(error_message);
+        } else if (_amountPaid > minimum_payment_amount && _amountPaid < minimum_payment_amount * 2) {
+            // Thanks for the tip event..
+        } else if (_amountPaid > (minimum_payment_amount * 2)) {
+            string memory error_message =
+            string(
+                bytes.concat(
+                    bytes("You payed to much, payment cannot cannot exceed 200%, you payed: "),
+                    bytes(Strings.toString(_amountPaid * 100 / minimum_payment_amount)),
+                    bytes("%")
+                )
+            );
+            revert(error_message);
+        } else {
+            // TODO IDSME log event. You have made an appointment.. Thanks for your business.
+        }
+
+
+        for (uint i = 0; i < appointments.length; i++)
+        { // If over lap revert.
+            // What if exactly the same time... same haf our... what happens test this.
+            if (_startTime < appointments[i].startTime && _endTime > appointments[i].startTime)
+            {
+                revert('New appointment request overlaps start of already existing appointment');
+            }
+            else if (_startTime < appointments[i].endTime && _endTime > appointments[i].endTime)
+            {
+                revert('New appointment request overlaps end of already existing appointment');
+            }
+            else if (_startTime > appointments[i].startTime && _endTime < appointments[i].endTime)
+            {
+                revert('New appointment request overlaps as falls within the time of an already existing appointment');
+            }
+            else if (_startTime == appointments[i].startTime && _endTime == appointments[i].endTime)
+            {
+                revert('New appointment request starts and ends exactly with an existing already appointment');
+            }
+            else // pure else statement false.. fix this... as is strange bug in Solidity compiler?
+            {
+                // TODO IDSME ... if all of above don't apply then appointment can be made as we have room in agenda.
+                //revert('Unexpected condition.... reverting money to be safe.');
+            }
+        }
+        appointments.push(Appointment(_title, msg.sender, _startTime, _endTime, _amountPaid));
+        return appointments[appointments.length - 1];
+    }
+
+    function getAppointments() public view returns (Appointment[] memory) {
+        return appointments;
     }
 
     function getRate() public view returns (uint) {
--- test/test.js
+++ test/test.js
@@ -1,13 +1,21 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Calend3", function () {
-  it("Should return the new rate once it's changed", async function () {
-    const Contract = await ethers.getContractFactory("Calend3");
-    const contract = await Contract.deploy("13");
-    await contract.deployed();
 
-    expect(await contract.getRate()).to.equal("13");
+describe("Calend3", function () {
+  let Contract, contract;
+  let owner, addr1, addr2;
+
+  beforeEach(async function () {
+    [owner, addr1, addr2] = await ethers.getSigners();
+
+    Contract = await ethers.getContractFactory("Calend3");
+    contract = await Contract.deploy("1");
+    await contract.deployed();
+  });
+
+  it("Should return the new rate once it's changed", async function () {
+    expect(await contract.getRate()).to.equal("1");
 
     const tx = await contract.setRate("1313");
 
@@ -19,14 +27,90 @@ describe("Calend3", function () {
 
 
   it("Should be blocked from setting rate as msg is not owner", async function () {
-    const Contract = await ethers.getContractFactory("Calend3");
-    const contract = await Contract.deploy(13);
-    await contract.deployed();
+    await expect(contract.connect(addr1).setRate("1")).to.be.revertedWith("Only owner can change rate");
+  });
 
-    // Change to different user account
-    const [owner, user1, user2] = await ethers.getSigners();
+  it("Should fail, as appointment requester did not pay enough", async function () {
+    await expect(contract.createAppointment("Meeting with Part Time Ids", 0, 60, 59)).to.be.revertedWith("You need to pay at least: 60, you tried to pay: 59 WEI");
+  });
 
-    await expect(contract.connect(user2).setRate("1313")).to.be.revertedWith("Only owner can change rate");
+  it("Should add 1 as appointment requester payed exactly the right amount, but no tip", async function () {
+    const tx = await contract.createAppointment("Meeting with Part Time Ids", 0, 60, 60);
+    await tx.wait();
+
+    const appointments = await contract.getAppointments();
+
+    expect(appointments.length).to.equal(1);
+  });
+
+  it("Should fail as appointment requester payed more then fee + tip, thus far to much. If more then (200%) is payed, requester probably made a typo, which will lead to add manual intervention if we don't rollback.", async function () {
+    await expect(contract.createAppointment("Meeting with Part Time Ids", 0, 60, 121)).to.be.revertedWith("You payed to much, payment cannot cannot exceed 200%, you payed: 201%");
+  });
+
+  it("Should, be able to tip 100% + max 100% tip, thanks ;)", async function () {
+    const tx2 = await contract.createAppointment("Breakfast at Tiffany's", 120, 240, 120);
+    await tx2.wait();
+
+    const appointments = await contract.getAppointments();
+    expect(appointments.length).to.equal(1);
+  });
+
+  it("Should add two appointments as appointments don't overlap", async function () {
+    const tx = await contract.createAppointment("Meeting with Part Time Ids", 0, 60, 60);
+    await tx.wait();
+
+    const tx2 = await contract.createAppointment("Breakfast at Tiffany's", 120, 240, 120);
+    await tx2.wait();
+
+    const appointments = await contract.getAppointments();
+
+    expect(appointments.length).to.equal(2);
+  });
+
+  it("Should add first appointment but not second one as overlaps start time", async function () {
+    const tx = await contract.createAppointment("Meeting with Part Time Ids", 30, 60, 30);
+    await tx.wait();
+
+    await expect(contract.createAppointment("Meeting with Part Time Larry", 29, 31, 2)).to.be.revertedWith("New appointment request overlaps start of already existing appointment");
+
+    const appointments = await contract.getAppointments();
+
+    expect(appointments.length).to.equal(1);
+  });
+
+
+  it("Should add first appointment but not second one as overlaps end time", async function () {
+    const tx = await contract.createAppointment("Meeting with Part Time Ids", 30, 60, 30);
+    await tx.wait();
+
+    await expect(contract.createAppointment("Meeting with Part Time Larry", 59, 61, 2)).to.be.revertedWith("New appointment request overlaps end of already existing appointment");
+
+    const appointments = await contract.getAppointments();
+
+    expect(appointments.length).to.equal(1);
+  });
+
+
+  it("Should add first appointment but not second one as falls withing existing appointment", async function () {
+    const tx1 = await contract.createAppointment("Meeting with Part Time Ids", 30, 60, 30);
+    await tx1.wait();
+
+    await expect(contract.createAppointment("Meeting with Part Time Larry", 31, 59, 28)).to.be.revertedWith("New appointment request overlaps as falls within the time of an already existing appointment");
+
+    const appointments = await contract.getAppointments();
+
+    expect(appointments.length).to.equal(1);
+  });
+
+  it("Should add first appointment but not second one as surrounds an existing appointment", async function () {
+    const tx1 = await contract.createAppointment("Meeting with Part Time Ids", 30, 60, 30);
+    await tx1.wait();
+
+    await expect(contract.createAppointment("Meeting with Part Time Larry", 29, 59, 32)).to.be.revertedWith("New appointment request overlaps start of already existing appointment");
+
+    const appointments = await contract.getAppointments();
+
+    expect(appointments.length).to.equal(1);
   });
 
 });
