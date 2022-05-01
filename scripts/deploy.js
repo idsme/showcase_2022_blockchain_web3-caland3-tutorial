2ce076a34964cb4b491606fce88d4bd4c9703a0c
idsme <idsmesales@gmail.com>
2022-05-02 00:25:10 +0200

    feature/implemented updated deployment script now copies abi to frontend dir

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,37 @@
- // We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+    const Contract = await hre.ethers.getContractFactory("Calend3");
+    const contract = await Contract.deploy(13);
 
-  // We get the contract to deploy
-  const Contract = await hre.ethers.getContractFactory("Calend3");
-  const contract = await Contract.deploy(13);
+    await contract.deployed();
+    const rate = await contract.getRate();
 
-  await contract.deployed();
+    console.log("Calend3 deployed to:", contract.address, rate);
 
-  console.log("Calend3 deployed to:", contract.address);
+    saveFrontendFiles();
 }
+
+function saveFrontendFiles() {
+    const fs = require("fs");
+
+    const abiDir = __dirname + "/../frontend/src/abis";
+
+    if (!fs.existsSync(abiDir)) {
+        fs.mkdirSync(abiDir);
+    }
+
+    const artifact = artifacts.readArtifactSync("Calend3");
+
+    fs.writeFileSync(
+        abiDir + "/Calend3.json",
+        JSON.stringify(artifact, null, 2)
+    );
+    console.log("Saved Calend3 ABI to:", abiDir + "/Calend3.json");
+}
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
@@ -30,3 +41,7 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+
+
+
